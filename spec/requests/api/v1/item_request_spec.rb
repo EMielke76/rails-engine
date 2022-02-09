fac307603da3b70855af82c8b1679234ae87a3dc
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-02-08 17:28:25 -0700

    Feat: All merchant specs passing, create item specs

---
+++ spec/requests/api/v1/item_request_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'the items endpoints' do
+  it 'sends information on all items' do
+    create_list(:items, 5)
+    
+  end
+
+  it 'sends information about a single item' do
+  end
+
+  it 'can create an item' do
+  end
+
+  it 'allows for an item to be edited' do
+  end
+
+  it 'allows for an item to be deleted' do
+  end
+
+  it 'returns merchant data for a given item' do
+  end
+end
