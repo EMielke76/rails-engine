9f990beb30aab208f52bf258171e50501477e722
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-02-10 11:35:46 -0700

    Add before_action functionality to controllers

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,15 +1,16 @@
 class Api::V1::ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :update, :destroy]
 
   def index
     render json: ItemSerializer.new(Item.all)
   end
 
   def show
-    render json: ItemSerializer.new(Item.find(params[:id]))
+    render json: ItemSerializer.new(@item)
   end
 
   def update
-    item = Item.update(params[:id], item_params)
+    item = Item.update(@item.id, item_params)
     if item.save
       render json: ItemSerializer.new(item), status: :ok
     else
@@ -27,11 +28,15 @@ class Api::V1::ItemsController < ApplicationController
   end
 
   def destroy
-    Item.find(params[:id]).destroy
+    @item.destroy
   end
 
   private
   def item_params
     params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,9 +1,16 @@
 class Api::V1::MerchantsController < ApplicationController
+  before_action :set_merchant, only: [:show]
+
   def index
     render json: MerchantSerializer.new(Merchant.all)
   end
 
   def show
-    render json: MerchantSerializer.new(Merchant.find(params[:id]))
+    render json: MerchantSerializer.new(@merchant)
+  end
+
+  private
+  def set_merchant
+    @merchant = Merchant.find(params[:id])
   end
 end
