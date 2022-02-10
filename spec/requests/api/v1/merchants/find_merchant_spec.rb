e188b427805fd06ba6ca96272217c4f0135dbd38
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-02-10 14:50:22 -0700

    Create test for Find A Merchant endpoint

---
+++ spec/requests/api/v1/merchants/find_merchant_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Find a Merchant endpoint' do
+  context 'happy path' do
+    it 'allows for searches by name' do
+    end
+
+    it 'returns a single object' do
+    end
+
+    it 'returns the first object in the database if multiple matches found' do
+    end
+
+  end
+
+  context 'sad path' do
+    it 'errors if a parameter is missing' do
+    end
+
+    it 'errors if a parameter is empty' do
+    end
+  end
+end 
