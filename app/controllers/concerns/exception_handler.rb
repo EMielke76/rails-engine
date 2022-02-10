cd43bb9f36170205ce82f303878f8c43c7c11d5c
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-02-10 14:22:46 -0700

    Add custom message to exception handlers

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -3,11 +3,11 @@ module ExceptionHandler
 
   included do
     rescue_from ActiveRecord::RecordNotFound do |e|
-      json_response({ message: e.message}, :not_found)
+      json_response({error: "Record Not Found", message: e.message}, :not_found)
     end
 
     rescue_from ActiveRecord::RecordInvalid do |e|
-      json_response({ message: e.message }, :unprocessable_entity)
+      json_response({ error: "Record Invalid", message: e.message }, :unprocessable_entity)
     end
   end
 end
